2a9fea6dc2c0b971c50a7e80dfffefdcab93525e
Sean R Reid <sean@torchcodelab.com>
2024-02-01 07:46:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Discogs V2 Starter
+
+This repo follows up on the intial work with the Discogs API.
+
+* It takes an artist id (defined in `js/index.js`) and renders their name to the DOM
+* It performs a lookup for artist releases, based on the url returned from the artist lookup
+* It includes the [Bulma CSS Framework](https://bulma.io/documentation/overview/) to provide some overall styling
+* It includes some basic styles in `css/styles.css` to override the Bulma defaults and customize the page
+
+This starter will be the baseline for in-class work.
---
+++ js/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('CONTENT LOADED');
+    const greeting = document.createElement('h1');
+    const root = document.querySelector('#root');
+
+    greeting.textContent = 'Discogs API Lookup';
+    root.append(greeting);
+
+    // We're defining a default, but this will change!
+    let artistId = '219213';
+
+    function get(url) {
+        return fetch(url, {
+            method: 'GET',
+            headers: {
+                'User-Agent': 'SeanIsRad/3.0',
+            },
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                return data;
+            });
+    }
+
+    function showArtist(artistNam) {
+        const artistHeader = document.createElement('h2');
+        artistHeader.textContent = artistNam;
+        root.appendChild(artistHeader);
+    }
+
+    function getReleases(url) {
+        get(url).then(function (data) {
+            // Destructure the releases
+            const { releases } = data;
+            // Create  UL
+            const list = document.createElement('ul');
+            // Append it to the #root
+            root.appendChild(list);
+
+            // Loop through the releases array
+            releases.map(function (release) {
+                // Create a list item
+                const listItem = document.createElement('li');
+                // Add the release title to the list item
+                listItem.textContent = `${release.title} -  ${release.year}`;
+                // Append the lisi item to the list
+                list.appendChild(listItem);
+            });
+        });
+    }
+
+    // This is an Immediately Invoked Function Expression aka IIFE (iffy)
+    (function () {
+        get(`https://api.discogs.com/artists/${artistId}`).then(function (data) {
+            // Destructure our data
+            const { name, releases_url } = data;
+            // Call it back
+            showArtist(name);
+            getReleases(releases_url);
+        });
+    })();
+});
